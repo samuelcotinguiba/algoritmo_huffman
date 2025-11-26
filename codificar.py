c97b42e6a60ae037bd456f0fb985f75dbcb56792
samuelcotinguiba <samuelcotinguiba@gmail.com>
2025-11-26 20:58:02 -0300

    Remover arquivos duplicados e temporários

--- codificar.py
+++
@@ -1,6 +0,0 @@
-def codificar(texto, codigos):
-    """
-    Entrada: texto e dicionário de códigos
-    Saída: string binária codificada
-    """
-    return ''.join(codigos[ch] for ch in texto)
--- contar_frequencias.py
+++
@@ -1,9 +0,0 @@
-def contar_frequencias(texto):
-    """
-    Entrada: string
-    Saída: dicionário {caractere: frequência}
-    """
-    freqs = {}
-    for ch in texto:
-        freqs[ch] = freqs.get(ch, 0) + 1
-    return freqs
